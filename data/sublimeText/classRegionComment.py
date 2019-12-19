e16fd59ef45213dd991252a6f019816cb717432b
ondratra <ondratra@gmail.com>
2019-12-19 22:11:57 +0100

    sublime text - comment making plugin update

--- data/sublimeText/classRegionComment.py
+++ data/sublimeText/classRegionComment.py
@@ -1,7 +1,9 @@
 import sublime
 import sublime_plugin
+import os
 
 brushCharacter = "/"
+brushCharacterCss = "*"
 preSize = 20
 postSize = 60
 textOffset = 1
@@ -9,9 +11,14 @@ textOffset = 1
 class ClassRegionCommentCommand(sublime_plugin.TextCommand):
 
     def run(self, edit):
+        extension = os.path.splitext(self.view.file_name())[1]
+
         for region in self.view.sel():
             line = self.view.line(region);
-            self.processLine(edit, line)
+            if extension == ".css":
+                self.processLineCss(edit, line)
+            else:
+                self.processLine(edit, line)
 
     def processLine(self, edit, line):
         originalLineContent = self.view.substr(line) # the text within the 'Region'
@@ -19,8 +26,17 @@ class ClassRegionCommentCommand(sublime_plugin.TextCommand):
         leadingSpaces = len(originalLineContent) - len(originalLineContent.lstrip())
 
 
-        if (len (lineContent) and postSize > len(lineContent)):
+        if (len(lineContent) and postSize > len(lineContent)):
             lineContent = " " * leadingSpaces + brushCharacter * (preSize - textOffset) + " " * textOffset + lineContent + " " * textOffset + brushCharacter * (postSize - len(lineContent) - textOffset - leadingSpaces)
 
-            #print("asdf", postSize, len(lineContent), lineContent)
+            self.view.replace(edit, line, lineContent)
+
+    def processLineCss(self, edit, line):
+        originalLineContent = self.view.substr(line) # the text within the 'Region'
+        lineContent = originalLineContent.strip(" *" + brushCharacter);
+        leadingSpaces = len(originalLineContent) - len(originalLineContent.lstrip())
+
+        if (len(lineContent) and postSize > len(lineContent)):
+            lineContent = " " * leadingSpaces + brushCharacter + brushCharacterCss * (preSize - textOffset - 1) + " " * textOffset + lineContent + " " * textOffset + brushCharacterCss * (postSize - len(lineContent) - textOffset - leadingSpaces - 1) + brushCharacter
+
             self.view.replace(edit, line, lineContent)
