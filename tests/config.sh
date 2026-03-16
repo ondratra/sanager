807903843b22c338dd2f73a4b5d2f47cc9b29d39
ondratra <ondratra@gmail.com>
2026-03-16 11:29:28 +0100

    tests - installation medium version bump

--- tests/config.sh
+++ tests/config.sh
@@ -2,7 +2,7 @@ SANAGER_TESTS_CONFIG_FOLDER=`realpath $(dirname "${BASH_SOURCE[0]}")`
 SANAGER_MAIN_DIR=`realpath $(dirname $SANAGER_TESTS_CONFIG_FOLDER)`
 
 # config
-DEBIAN_VERSION="13.3.0"
+DEBIAN_VERSION="13.4.0"
 NOWADAYS_DEBIAN_VERSION="trixie"
 #DEBIAN_MIRROR_FTP="ftp.cz.debian.org/debian/"
 DEBIAN_MIRROR_FTP="ftp.us.debian.org/debian/"
