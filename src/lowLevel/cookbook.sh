bf976589e9de479ab1909a5eaf11cf0f1561a364
ondratra <ondratra@gmail.com>
2026-06-06 04:02:55 +0200

    kernel version bump - sid - 7.0.10+deb14 + stable - 6.12.90+deb13

--- src/lowLevel/cookbook.sh
+++ src/lowLevel/cookbook.sh
@@ -622,7 +622,7 @@ function pkg_multimedia_necessary {
 }
 
 function pkg_newestLinuxKernel {
-    local KERNEL_VERSION=$(is_debian_sid && echo "7.0.9+deb14" || echo "6.12.74+deb13+1")
+    local KERNEL_VERSION=$(is_debian_sid && echo "7.0.10+deb14" || echo "6.12.90+deb13")
     local PACKAGES="linux-image-$KERNEL_VERSION-amd64 linux-headers-$KERNEL_VERSION-amd64"
 
     aptGetInstall $PACKAGES
